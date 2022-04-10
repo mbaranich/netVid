551fa271245042c2479103f3503081d9317eac11
Matthew Baranich <mbaranich@yahoo.com>
2022-04-09 20:42:52 -0400

    Add routing to movie pages.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
+import MovieForm from './components/movieForm';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
@@ -14,6 +15,7 @@ function App() {
     <NavBar />
     <main className='container'>
     <Switch>
+      <Route path="/movies/:id" component={MovieForm} />
       <Route path="/movies" component={Movies}></Route>
       <Route path="/customers" component={Customers}></Route>
       <Route path="/rentals" component={Rentals}></Route>
