e360383904566000801e536efd56d206f133624c
Matthew Baranich <mbaranich@yahoo.com>
2022-04-09 20:34:33 -0400

    Add router & navbar.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
-import './App.css';
+import { Route, Redirect, Switch } from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import Rentals from './components/rentals';
+import NotFound from './components/notFound';
+import NavBar from './components/navBar';
+import './App.css';
+import React from 'react';
 
 function App() {
   return (
+    <React.Fragment>
+    <NavBar />
     <main className='container'>
-      <Movies />
+    <Switch>
+      <Route path="/movies" component={Movies}></Route>
+      <Route path="/customers" component={Customers}></Route>
+      <Route path="/rentals" component={Rentals}></Route>
+      <Route path="/notFound" component={NotFound}></Route>
+      <Redirect from="/" exact to="/movies" />
+      <Redirect to="/notFound" />
+      </Switch>
     </main>
+    </React.Fragment>
   );
 }
 
