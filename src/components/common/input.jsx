4c75b7530579ea7649ec25c6bd0977540937aae8
Matthew Baranich <mbaranich@yahoo.com>
2022-04-16 20:50:27 -0400

    Add validation using joi-browser.

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 
-const Input = ({ name, label, value, onChange }) => {
+const Input = ({ name, label, value, error, onChange }) => {
     return ( 
     <div className="form-group">
         <label htmlFor={name}>{label}</label>
         <input value={value} name={name} onChange={onChange} id={name}  type="text" className="form-control" autoFocus />
+        {error && <div className="alert alert-danger">
+            {error}
+        </div>}
     </div>
      );
 }
