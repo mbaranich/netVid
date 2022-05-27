9207b892bcfa6a51e41f18b135f6990ff961646f
Matthew Baranich <mbaranich@yahoo.com>
2022-05-26 20:35:54 -0400

    Display username, add logout.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import jwtDecode from 'jwt-decode'
 import Movies from './components/movies';
 import MovieForm from './components/movieForm';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
 import NotFound from './components/notFound';
 import NavBar from './components/navBar';
-import './App.css';
-import React from 'react';
-import { ToastContainer } from 'react-toastify';
 import LoginForm from './components/loginForm';
 import RegisterForm from './components/registerForm';
 import NewMovie from './components/movieForm';
+import Logout from './components/logout';
+import logo from './logo.svg';
+import './App.css';
 import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+class App extends Component {
+  state = {} ;
+  
+ componentDidMount() {
+  try{
+    const jwt = localStorage.getItem('token');
+    const user = jwtDecode(jwt)
+    this.setState({ user })
+  }
+  catch(ex) {}
+ }
+
+ render() {
   return (
     <React.Fragment>
     <ToastContainer />
-    <NavBar />
+    <NavBar user={this.state.user} />
     <main className='container'>
     <Switch>
-      <Route path="/login" component={LoginForm} />
       <Route path="/register" component={RegisterForm} />
+      <Route path="/login" component={LoginForm} />
+      <Route path="/logout" component={Logout} />
       <Route path="/movies/:id" component={MovieForm} />
       <Route path="/movies/new" component={NewMovie} />
       <Route path="/movies" component={Movies}></Route>
@@ -35,6 +50,7 @@ function App() {
     </main>
     </React.Fragment>
   );
+ }
 }
 
 export default App;
